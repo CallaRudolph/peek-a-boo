8353f9a7ae28aec8fc720e093c5f60c1bc9219cc
Benjamin Hampikian, Calla Rudolph and Max Scher <benjamin@>
2017-05-30 15:25:02 -0700

    duplicate toggle functions to add giraffe section

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 $(function() {
-  $(".clickable").click(function() {
+  $(".clickableW").click(function() {
     $("#walrus-showing").fadeToggle();
     $("#walrus-hidden").fadeToggle();
+  });
+});
+
+//
+// $(function() {
+//   $(".borderfun").click(function() {
+//     $("#img-border").slideToggle();
+//     $("#img-noborder").slideToggle();
+//   })
+// })
+
+$(function() {
+  $(".clickableG").click(function() {
+    $("#giraffe-showing").slideToggle();
+    $("#giraffe-hidden").slideToggle();
   });
 });
